c72a329cc717949d7551bf0c5e4ae018e8786a9a
Andres Gutierrez <agutierrezt@unal.edu.co>
2014-11-26 03:35:16 -0500

    Video de howToUse

--- UNFleaPlus/grails-app/assets/javascripts/modal.js
+++ UNFleaPlus/grails-app/assets/javascripts/modal.js
@@ -1,7 +1,7 @@
 $(function() {
 	
 	$('#myModal').on('shown.bs.modal', function(){
-		$('#myModal .modal-body').html('<iframe width="100%" height="350" src="//www.youtube.com/embed/IBH97ma9YiI"></iframe>');
+		$('#myModal .modal-body').html('<iframe width="100%" height="350" src="//www.youtube.com/embed/ybgOv-k5mOk"></iframe>');
     });
     $('#myModal').on('hidden.bs.modal', function(){
     	$('#myModal .modal-body').html('');
